90b7ee507472c619c563d821601ecf564decc21f
Karan Thakar <karanthakar2834@gmail.com>
2025-07-06 23:22:21 +0530

    check1

--- frontend/src/pages/Cars.jsx
+++ frontend/src/pages/Cars.jsx
@@ -1,6 +1,95 @@
 import { motion } from 'framer-motion';
-import { cars } from '../data/cars';
+// import { cars } from '../data/cars';
 import CarCard from '../components/CarCard';
+import crysta from '../assets/tyuuhab_1655937.avif';
+import ertiga from '../assets/suzuki-ertiga-hybrid-color-799385.avif';
+import dzire from '../assets/maruti-suzuki-swift-dzire-magma-grey.png';
+import baleno from '../assets/baleno.png';
+import xl6 from '../assets/XL6-Front-3_4th..webp';
+import aura from '../assets/vs6fq0b_1639129.avif';
+import urbania from '../assets/Force_Urbania_front_2022_11_23_T08_07_52_722_Z_62a15f1e7f.jpg';
+import traveller from '../assets/traveller-11.png';
+
+export const cars = [
+  {
+    id: 1,
+    name: "Innova Crysta",
+    capacity: 7,
+    image: crysta,
+    phone: "+919106661466",
+  },
+  {
+    id: 2,
+    name: "Ertiga",
+    capacity: 6,
+    image: ertiga,
+    phone: "+919106661466",
+  },
+  {
+    id: 3,
+    name: "Swift Dzire",
+    capacity: 4,
+    image: dzire,
+    phone: "+919106661466",
+  },
+  {
+    id: 4,
+    name: "Baleno",
+    capacity: 5,
+    image: baleno,
+    phone: "+919106661466",
+  },
+  {
+    id: 5,
+    name: "Nexa XL6",
+    capacity: 7,
+    image: xl6,
+    phone: "+919106661466",
+  },
+  {
+    id: 6,
+    name: "AURA",
+    capacity: 5,
+    image: aura,
+    phone: "+919106661466",
+  },
+  {
+    id: 7,
+    name: "Urbania",
+    capacity: 13,
+    image: urbania,
+    phone: "+919106661466",
+  },
+  {
+    id: 8,
+    name: "Urbania",
+    capacity: 17,
+    image: urbania,
+    phone: "+919106661466",
+  },
+  {
+    id: 9,
+    name: "Tempo Traveller",
+    capacity: 14,
+    image: traveller,
+    phone: "+919106661466",
+  },
+  {
+    id: 10,
+    name: "Tempo Traveller",
+    capacity: 26,
+    image: traveller,
+    phone: "+919106661466",
+  },
+  {
+    id: 11,
+    name: "Tempo Traveller",
+    capacity: 29,
+    image: traveller,
+    phone: "+919106661466",
+  },
+];
+
 
 function Cars() {
   return (
